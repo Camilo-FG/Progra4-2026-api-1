277609d6816254ad8003c45e63d8119c2bd58e91
Elian Fajardo Gutierrez <eliancfg.fajardo@gmail.com>
2026-05-07 19:31:05 -0600

    add pokemon

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Progra4_2026_api_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        // GET: api/<PokemonController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Pikachu", "Charmander", "Bulbasaur" };
+        }
+
+     
+    }
+}
